2ec784bbd024fef8c0761ebe7eef1ae5f5f0cb88
Lastraeus <>
2023-08-24 18:28:44 +0000

    add: add to bag functinality

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'p@ei#69*b*zz3u4yie-$()@cy^l(+x9&@6ypx+r0lm(3%_9hr7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8001-lastraeus-boutique-ado-p-7ubk57e81j.us2.codeanyapp.com']
+ALLOWED_HOSTS = ['8000-lastraeus-boutique-ado-p-7ubk57e81j.us2.codeanyapp.com']
 
 
 # Application definition
